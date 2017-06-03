700ddf11bdae624ecc56fb9ab5c5cbc70c50c385
Anna Fleming <afleming@petsafe.net>
2017-06-03 19:32:31 -0400

    Add Python Primer Creativity exercises solutions

--- Python Primer/exercises/creativity.py
+++ Python Primer/exercises/creativity.py
@@ -1,3 +1,16 @@
 
 def reverse_list(list_of_ints):
     return [list_of_ints[len(list_of_ints) - i - 1] for i in range(len(list_of_ints))]
+
+
+def has_product_odd(sequence):
+    for i in sequence:
+        for j in sequence:
+            if j * i % 2 == 1 and i != j:
+                return True
+    return False
+
+
+def are_numbers_unique(sequence):
+    return len(set(sequence)) == len(sequence)
+
--- Python Primer/exercises/tests/creativity_test.py
+++ Python Primer/exercises/tests/creativity_test.py
@@ -9,5 +9,18 @@ class CreativityExercisesTestCase(unittest.TestCase):
         expected = [5, 4, 3, 2, 1]
         self.assertEqual(creativity.reverse_list(list_of_int), expected)
 
+    def test_has_product_odd_in_sequence(self):
+        list_of_int = [1, 2, 3, 4, 5]
+        self.assertTrue(creativity.has_product_odd([1, 2, 3, 4, 5]))
+        self.assertFalse(creativity.has_product_odd([2, 4, 4, 6]))
+
+    def test_has_product_odd_in_sequence_from_two_distinct_numbers(self):
+        self.assertFalse(creativity.has_product_odd([3, 3, 2]))
+
+    def test_should_determines_if_numbers_in_sequence_are_unique(self):
+        self.assertTrue(creativity.are_numbers_unique([1, 2, 3, 4, 5]))
+        self.assertFalse(creativity.are_numbers_unique([1, 2, 2, 4, 5]))
+
+
 if __name__ == '__main__':
     unittest.main()
