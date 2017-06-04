9a147a497c3caa6907eef0949f3c772e1e0e95aa
Anna Fleming <afleming@petsafe.net>
2017-06-04 16:22:16 -0400

    Solve make change task using generators

--- Python Primer/exercises/projects.py
+++ Python Primer/exercises/projects.py
@@ -10,3 +10,18 @@ def make_change(charged, given):
             change.append(bill)
             left -= bill
     return change
+
+
+def make_change_with_generator(charged, given):
+    return [i for i in _change_generator(charged, given)]
+
+
+def _change_generator(charged, given):
+    bills = [100, 50, 20, 10, 5, 1]
+    left = given - charged
+    for bill in bills:
+        if not left:
+            break
+        while left >= bill:
+            yield bill
+            left -= bill
--- Python Primer/exercises/tests/projects_test.py
+++ Python Primer/exercises/tests/projects_test.py
@@ -4,9 +4,12 @@ from .. import projects
 
 class ProjectsExercisesTestCase(unittest.TestCase):
 
-    def test_should_reverse_list(self):
+    def test_should_make_change(self):
         self.assertEqual(projects.make_change(35, 50), [10, 5])
         self.assertEqual(projects.make_change(1, 50), [20, 20, 5, 1, 1, 1, 1])
 
+    def test_should_make_change_using_generator(self):
+        self.assertEqual(projects.make_change_with_generator(35, 50), [10, 5])
+        self.assertEqual(projects.make_change_with_generator(1, 50), [20, 20, 5, 1, 1, 1, 1])
 if __name__ == '__main__':
     unittest.main()
