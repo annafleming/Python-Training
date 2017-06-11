fe297788e60f5cc1ca774339a2832f8966202c13
Anna Fleming <afleming@petsafe.net>
2017-06-11 15:51:00 -0400

    OOP R-2.22

--- Object-Oriented Programming/exercises/reinforcement/sequence.py
+++ Object-Oriented Programming/exercises/reinforcement/sequence.py
@@ -23,3 +23,11 @@ class Sequence(metaclass=ABCMeta):
         for j in range(len(self)):
             if self[j] == val:
                 return k
+
+    def __eq__(self, other):
+        if len(self) != len(other):
+            return False
+        for i in len(self):
+            if self[i] != other[i]:
+                return False
+        return True
