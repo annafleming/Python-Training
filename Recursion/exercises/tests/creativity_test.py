bff5c316414c5a7496de0531a4edbfc9c1b07f61
Anna Fleming <afleming@petsafe.net>
2017-06-28 22:26:19 -0400

    Add test for recursion exercises

---
+++ Recursion/exercises/tests/creativity_test.py
@@ -0,0 +1,12 @@
+import unittest
+from .. import creativity
+
+
+class CreativityExercisesTestCase(unittest.TestCase):
+
+    def test_should_return_min_max_value_in_sequence(self):
+        sequence = [2, 1, 5, 4, 3]
+        self.assertEqual(creativity.get_min_max(sequence), (1, 5))
+
+if __name__ == '__main__':
+    unittest.main()
