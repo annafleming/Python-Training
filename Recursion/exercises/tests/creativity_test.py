96be2643941acf542a22fff6a7565ea092ce9bfc
Anna Fleming <afleming@petsafe.net>
2017-06-28 22:55:21 -0400

    Recursion C-4.9

--- Recursion/exercises/tests/creativity_test.py
+++ Recursion/exercises/tests/creativity_test.py
@@ -8,5 +8,11 @@ class CreativityExercisesTestCase(unittest.TestCase):
         sequence = [2, 1, 5, 4, 3]
         self.assertEqual(creativity.get_min_max(sequence), (1, 5))
 
+        sequence = [1]
+        self.assertEqual(creativity.get_min_max(sequence), (1, 1))
+
+        sequence = [5, 5, 5, 6]
+        self.assertEqual(creativity.get_min_max(sequence), (5, 6))
+
 if __name__ == '__main__':
     unittest.main()
