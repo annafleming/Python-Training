752d1640c6b55e32642c831826ff84727b7c00e6
Anna Fleming <afleming@petsafe.net>
2017-07-08 15:27:30 -0400

    Python stash implementation

---
+++ Stacks, Queues, and Deques/array_stack.py
@@ -0,0 +1,26 @@
+class Empty(Exception):
+    pass
+
+
+class ArrayStack:
+    def __init__(self):
+        self.data = []
+
+    def __len__(self):
+        return len(self.data)
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def push(self, e):
+        self.data.append(e)
+
+    def top(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self.data[-1]
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self.data.pop()
