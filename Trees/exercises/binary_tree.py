2ab81d302981317c9c6519fac91e83c3fe60fb12
Anna Fleming <afleming@petsafe.net>
2017-07-26 09:07:09 -0400

    Inorder traversal implementaion for Binary tree

--- Trees/exercises/binary_tree.py
+++ Trees/exercises/binary_tree.py
@@ -23,3 +23,21 @@ class BinaryTree(Tree):
             yield self.left(p)
         if self.right(p) is not None:
             yield self.right(p)
+
+    def positions(self):
+        return self.inorder()
+
+    def inorder(self):
+        if not self.is_empty():
+            for p in self._subtree_inorder(self.root()):
+                yield p
+
+    def _subtree_inorder(self, p):
+        if self.left(p) is not None:
+            for other in self._subtree_inorder(p):
+                yield other
+        yield p
+        if self.right(p) is not None:
+            for other in self._subtree_inorder(p):
+                yield other
+
