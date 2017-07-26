01b8721efe23e6c8e653baef58129a12aef8a2b1
Anna Fleming <afleming@petsafe.net>
2017-07-26 09:06:43 -0400

    Tree traversal algorithm implementation

--- Trees/exercises/tree.py
+++ Trees/exercises/tree.py
@@ -40,3 +40,44 @@ class Tree:
         else:
             return 1 + self.depth(self.parent(p))
 
+    def __iter__(self):
+        for p in self.positions():
+            yield p.element()
+
+    def positions(self):
+        return self.preorder()
+
+    def preorder(self):
+        if not self.is_empty():
+            for p in self._subtree_preorder(self.root()):
+                yield p
+
+    def _subtree_preorder(self, p):
+        yield p
+        for c in self.children(p):
+            for other in self._subtree_preorder(c):
+                yield other
+
+    def postorder(self):
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):
+                yield p
+
+    def _subtree_postorder(self, p):
+        for c in self.children(p):
+            for other in self._subtree_postorder(c):
+                yield other
+        yield p
+
+    def breadthfirst(self):
+        if not self.is_empty():
+            fringe = LinkedQueue()
+        fringe.enqueue(self.root())
+        while not fringe.is_empty():
+            p = fringe.dequeue()
+            yield p
+            for c in self.children(p):
+                fringe.enqueue(c)
+
+
+
