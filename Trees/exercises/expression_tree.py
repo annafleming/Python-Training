2143008374d55bba0430632ace18797ee6375705
Anna Fleming <afleming@petsafe.net>
2017-08-01 15:53:46 -0400

    Implement Expression Tree

---
+++ Trees/exercises/expression_tree.py
@@ -0,0 +1,61 @@
+from .linked_binary_tree import LinkedBinaryTree
+
+
+class ExpressionTree(LinkedBinaryTree):
+    def __init__(self, token, left=None, right=None):
+        super().__init__()
+        if not isinstance(token, str):
+            raise TypeError('Token must be a string')
+        self._add_root(type)
+        if left is not None:
+            if token not in '+-*x/':
+                raise ValueError('token must be a valid operator')
+            self._attach(self.root(), left, right)
+
+    def __str__(self):
+        pieces = []
+        self._parenthesize_recur(self.root(), pieces)
+        return ''.join(pieces)
+
+    def _parenthesize_recur(self, p, result):
+        if self.is_leaf(p):
+            result.append(str(p.element()))
+        else:
+            result.append('(')
+            self._parenthesize_recur(self.left(p), result)
+            result.append(p.element())
+            self._parenthesize_recur(self.right(p), result)
+            result.append(')')
+
+    def evaluate(self):
+        return self._evaluate_recur(self.root())
+
+    def _evaluate_recur(self, p):
+        if self.is_leaf(p):
+            return float(p.element())
+        else:
+            op = p.element()
+            left_val = self._evaluate_recur(self.left(p))
+            right_val = self._evaluate_recur(self.right(p))
+            if op == '+':
+                return left_val + right_val
+            if op == '-':
+                return left_val - right_val
+            if op == '/':
+                return left_val / right_val
+            return left_val * right_val
+
+
+def build_expression_tree(tokens):
+    S = []
+    for t in tokens:
+        if t in '+-*x/':
+            S.append(t)
+        elif t not in '()':
+            S.append(ExpressionTree(t))
+        elif t == ')':
+            right = S.pop()
+            op = S.pop()
+            left = S.pop()
+            S.append(ExpressionTree(op, left, right))
+    return S.pop()
